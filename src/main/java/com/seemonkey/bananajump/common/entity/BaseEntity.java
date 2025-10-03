3878cd05c9c1c54c371c7ea8f65b16ba067d5ddf
5ooyeon <symelcho@inu.ac.kr>
2025-10-03 16:43:38 +0900

    chore: setup base entity, base response, exceptions

---
+++ src/main/java/com/seemonkey/bananajump/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.seemonkey.bananajump.common.entity;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	protected OffsetDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	protected OffsetDateTime updatedAt;
+
+}
---
+++ src/main/java/com/seemonkey/bananajump/common/response/BaseResponse.java
@@ -0,0 +1,80 @@
+package com.seemonkey.bananajump.common.response;
+
+import java.time.OffsetDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.seemonkey.bananajump.common.exception.ErrorType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseResponse<T> {
+	private int status;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private String code;          // "OK", "VALIDATION_ERROR", "NOT_FOUND" ...
+	private String message;       // 사용자 메시지
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private T data;
+	private OffsetDateTime timestamp;
+	private String traceId;
+
+	public static <T> ResponseEntity<BaseResponse<T>> ok(T data) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(BaseResponse.<T>builder()
+				.status(HttpStatus.OK.value())
+				.code(HttpStatus.OK.name())
+				.data(data)
+				.timestamp(OffsetDateTime.now())
+				.build());
+	}
+
+	public static <T> ResponseEntity<BaseResponse<T>> created(T data) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(BaseResponse.<T>builder()
+				.status(HttpStatus.CREATED.value())
+				.code(HttpStatus.CREATED.name())
+				.data(data)
+				.timestamp(OffsetDateTime.now())
+				.build());
+	}
+
+	public static <T> ResponseEntity<BaseResponse<T>> created() {
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(BaseResponse.<T>builder()
+				.status(HttpStatus.CREATED.value())
+				.code(HttpStatus.CREATED.name())
+				.timestamp(OffsetDateTime.now())
+				.build());
+	}
+
+	// 에러 응답 생성 메서드
+	public static ResponseEntity<BaseResponse<Void>> error(ErrorType errorType) {
+		return ResponseEntity.status(errorType.getStatus())
+			.body(BaseResponse.<Void>builder()
+				.status(errorType.getStatus().value())
+				.code(errorType.getErrorCode())
+				.message(errorType.getMessage())
+				.timestamp(OffsetDateTime.now())
+				.build());
+	}
+
+	public static <String> ResponseEntity<BaseResponse<String>> error(ErrorType errorType, String msg) {
+		return ResponseEntity.status(errorType.getStatus())
+			.body(BaseResponse.<String>builder()
+				.status(errorType.getStatus().value())
+				.code(errorType.getErrorCode())
+				.message(errorType.getMessage())
+				.data(msg)
+				.timestamp(OffsetDateTime.now())
+				.build());
+	}
+}
