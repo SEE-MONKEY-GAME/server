a84254391883f628200275fe1f77611008080343
5ooyeon <symelcho@inu.ac.kr>
2025-10-10 17:45:37 +0900

    feat: add Inventory entity

---
+++ src/main/java/com/seemonkey/bananajump/inventory/domain/Inventory.java
@@ -0,0 +1,31 @@
+package com.seemonkey.bananajump.inventory.domain;
+
+import com.seemonkey.bananajump.item.domain.Item;
+import com.seemonkey.bananajump.member.domain.Profile;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Inventory {
+
+	@EmbeddedId
+	private InventoryId id;
+
+	@MapsId("memberId")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "member_id")
+	private Profile profile;
+
+	@MapsId("itemId")
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "item_id")
+	private Item item;
+
+	@Column(name = "quantity", nullable = false)
+	private Long quantity;
+}
