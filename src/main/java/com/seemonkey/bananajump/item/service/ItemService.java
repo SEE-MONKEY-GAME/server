f743ab65301ea98635f4ca0ef4a1c82f28a66c36
5ooyeon <symelcho@inu.ac.kr>
2025-11-12 19:16:46 +0900

    refactor: 아이템 지급, 결제 차감 모듈 분리

--- src/main/java/com/seemonkey/bananajump/item/service/ItemService.java
+++ src/main/java/com/seemonkey/bananajump/item/service/ItemService.java
@@ -2,7 +2,9 @@ package com.seemonkey.bananajump.item.service;
 
 import java.util.List;
 
+import com.seemonkey.bananajump.item.domain.Item;
 import com.seemonkey.bananajump.item.dto.ItemDto;
+import com.seemonkey.bananajump.member.domain.Profile;
 
 public interface ItemService {
 	List<ItemDto.GetItemListResDto> getItemList(Long memberId);
@@ -10,4 +12,6 @@ public interface ItemService {
 	void buyItem(Long itemId, Long memberId, int quantity);
 
 	void useItem(Long itemId, Long memberId);
+
+	void addItem(Item item, Profile profile, int quantity);
 }
